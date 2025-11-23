e7ab0070ee144d1f0fe9bee556add774a95c63d8
Mustakim shaikh <mustakimshaikh@Mustakims-MacBook-Air.local>
2025-11-23 17:53:15 +0530

    initial commit

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import app from "./app.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`🚀 UniFlow backend running on http://localhost:${PORT}`);
